db9b55377212c6c36a354461751d8e081fe51871
meraj2704 <mh1669101@gmail.com>
2024-11-10 15:01:20 +0600

    home page api updated

--- src/app/modules/home/home.controllers.ts
+++ src/app/modules/home/home.controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { sendErrorResponse, sendSuccessResponse } from "../../utils/response";
 import { homeService } from "./home.service";
 import mongoose from "mongoose";
+import { HealthServices } from "../healthInfo/health.service";
 
 const getLatestUserData = async (req: Request, res: Response) => {
   const { user_id } = req.params;
@@ -9,22 +10,30 @@ const getLatestUserData = async (req: Request, res: Response) => {
   try {
     const { glucose, measurements, pressure } =
       await homeService.latestDataOfUser(id);
+    const todayGlucose = await HealthServices.glucoseByDays(id, 1);
+    const todayPressure = await HealthServices.pressureByDays(id, 1);
+    const bmiData = await homeService.bmiHomeData(id);
     const newData = {
       glucose: {
-        glucose:glucose?.glucose,
-        date:glucose?.date
+        glucose: glucose?.glucose,
+        todayGlucose: todayGlucose,
+        date: glucose?.date,
       },
       measurements: {
         height: measurements?.height,
         weight: measurements?.weight,
         bmi: measurements?.bmi,
-        date:measurements?.date
+        date: measurements?.date,
       },
       pressure: {
         low_pressure: pressure?.low_pressure,
         high_pressure: pressure?.high_pressure,
-        data:pressure?.date
+        todayPressure,
+        data: pressure?.date,
       },
+      averageBmi: bmiData.averageBmi,
+      maxBmi: bmiData.maxBmi,
+      minBmi: bmiData.minBmi,
     };
 
     return sendSuccessResponse(
--- src/app/modules/home/home.service.ts
+++ src/app/modules/home/home.service.ts
@@ -22,7 +22,20 @@ const latestDataOfUser = async (user_id: mongoose.Types.ObjectId) => {
   };
 };
 
+const bmiHomeData = async (user_id: mongoose.Types.ObjectId) => {
+  const measureMents = await BodyMeasurement.find({ user_id });
+  if (measureMents.length === 0) {
+    return { averageBmi: 0, minBmi: 0, maxBmi: 0 };
+  }
+  const bmiValues = measureMents.map((measurement) => measurement.bmi);
+  const totalBmi = bmiValues.reduce((sum, bmi) => sum + bmi, 0);
+  const averageBmi = parseFloat((totalBmi / bmiValues.length).toFixed(1));
+  const minBmi = Math.min(...bmiValues);
+  const maxBmi = Math.max(...bmiValues);
+  return { averageBmi, minBmi, maxBmi };
+};
 
-export const homeService ={
-    latestDataOfUser,
-}
+export const homeService = {
+  latestDataOfUser,
+  bmiHomeData,
+};
