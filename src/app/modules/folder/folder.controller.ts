6b52dab84e928ce28fe472f57f8efc939856727f
meraj2704 <mh1669101@gmail.com>
2025-01-13 23:18:28 +0600

    update code and changes

---
+++ src/app/modules/folder/folder.controller.ts
@@ -0,0 +1,7 @@
+const createFolder = async (req: Request, res: Response) => {
+    
+};
+
+export const FolderControllers = {
+  createFolder,
+};
--- src/app/modules/folder/folder.models.ts
+++ src/app/modules/folder/folder.models.ts
@@ -1,13 +1,14 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { IFolder } from "./folder.interface";
 
-const FolderSchema = new Schema<IFolder>({
+const FolderSchema = new Schema<IFolder>(
+  {
     user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    name: { type: string, required: true },
-    date: { type: Date, default: Date.now },
-  });
-  
-  export const Glucose = mongoose.model<GlucoseDocument>(
-    "Glucose",
-    GlucoseSchema
-  );
+    name: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Glucose = mongoose.model<IFolder>("Glucose", FolderSchema);
